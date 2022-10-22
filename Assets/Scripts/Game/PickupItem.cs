32f1de9264554cde05dd8d6690b9bfc389a89b49
Ryan McKeever <rkfmckee@gmail.com>
2022-10-22 21:35:42 +0100

    Improve Organisation

---
+++ Assets/Scripts/Game/PickupItem.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PickupItem : MonoBehaviour
+{
+	#region Events
+
+	private void Update()
+	{
+		if (Input.GetButtonDown("Fire1")) 
+		{
+			print("pressed");
+		}
+	}
+
+	#endregion
+
+	#region Methods
+
+
+
+	#endregion
+}
---
+++ Assets/Scripts/_StaticScripts/References.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class References
+{
+    public class UI 
+	{
+		public Canvas canvas;
+	}
+}
