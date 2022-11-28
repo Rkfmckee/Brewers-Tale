a16734b0291ee8d70fb374ac58a1a19950bab8d8
Ryan McKeever <rkfmckee@gmail.com>
2022-11-28 00:02:32 +0000

    Add ItemOptions UI menu

--- Assets/Scripts/UI/Hover/Hover.cs
+++ Assets/Scripts/UI/Hover/Hover.cs
@@ -10,6 +10,8 @@ public class Hover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 	private Sprite regularSprite;
 	[SerializeField]
 	private Sprite highlightSprite;
+	private Color highlightColour;
+	private Color regularColour;
 
 	private Image image;
 
@@ -20,16 +22,26 @@ public class Hover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 	private void Awake()
 	{
 		image = GetComponent<Image>();
+
+		var transparent = new Color(1, 1, 1, 0);
+		var white = new Color(1, 1, 1, 1);
+
+		highlightColour = highlightSprite == null ? transparent : white;
+		regularColour = regularSprite == null ? transparent : white;
+
+		image.color = regularColour;
 	}
 
 	public virtual void OnPointerEnter(PointerEventData eventData)
 	{
 		image.sprite = highlightSprite;
+		image.color = highlightColour;
 	}
 
 	public virtual void OnPointerExit(PointerEventData eventData)
 	{
 		image.sprite = regularSprite;
+		image.color = regularColour;
 	}
 
 	#endregion
