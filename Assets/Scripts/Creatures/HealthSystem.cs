597ef3a7ad5e0066d2fa601d62a647b459cf8d09
Ryan McKeever <rkfmckee@gmail.com>
2022-12-18 18:37:43 +0000

    Add HealthSystem and HealthBar

---
+++ Assets/Scripts/Creatures/HealthSystem.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField]
+	private float maxHealth;
+	[SerializeField]
+	private float heightOffset;
+
+	private float currentHealth;
+	private HealthBar healthBar;
+
+	#endregion
+
+	#region Properties
+
+	public float MaxHealth { get => maxHealth; set => maxHealth = value; }
+	public float HeightOffset { get => heightOffset; }
+
+	public float CurrentHealth
+	{
+		get => currentHealth;
+		set
+		{
+			currentHealth = value;
+			healthBar.UpdateHealthBar(currentHealth / maxHealth);
+		}
+	}
+
+	#endregion
+
+	#region Events
+
+	private void Start()
+	{
+		var healthBarPrefab = Resources.Load<GameObject>("Prefabs/UI/HealthBar");
+		var healthBarGroup = References.UI.Canvas.transform.Find("HealthBars");
+		healthBar = Instantiate(healthBarPrefab, healthBarGroup).GetComponent<HealthBar>();
+		healthBar.Character = this;
+
+		CurrentHealth = maxHealth;
+	}
+
+	#endregion
+
+	#region Methods
+
+	public void TakeDamage(float damage)
+	{
+		var newHealth = CurrentHealth - damage;
+		if (newHealth <= 0)
+		{
+			Destroy(gameObject);
+			return;
+		}
+
+		CurrentHealth = newHealth;
+	}
+
+	public void Heal(float health)
+	{
+		var newHealth = CurrentHealth + health;
+		if (newHealth >= MaxHealth)
+		{
+			CurrentHealth = MaxHealth;
+			return;
+		}
+
+		CurrentHealth = newHealth;
+	}
+
+	#endregion
+}
