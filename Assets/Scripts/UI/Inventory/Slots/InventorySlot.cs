b99c669c72baacfd7adc41bf439bb6f6adae9070
Ryan McKeever <rkfmckee@gmail.com>
2023-01-03 21:17:41 +0000

    Create inventory slots on load, and add Slot numbers

--- Assets/Scripts/UI/Inventory/Slots/InventorySlot.cs
+++ Assets/Scripts/UI/Inventory/Slots/InventorySlot.cs
@@ -1,4 +1,19 @@
 public class InventorySlot : InventoryCraftingSlot
 {
-	
+	#region Properties
+
+	public int SlotNumber { get; set; }
+
+	#endregion
+
+	#region Events
+
+	protected override void Awake()
+	{
+		base.Awake();
+
+		References.Inventory.Slots.Add(this);
+	}
+
+	#endregion
 }
--- Assets/Scripts/_HelperScripts/References.cs
+++ Assets/Scripts/_HelperScripts/References.cs
@@ -9,6 +9,11 @@ public static class References
 	public static List<EnemySpace> EnemySpaces = new List<EnemySpace>();
 	public static EnemySpawner EnemySpawner;
 
+	public static class Inventory
+	{
+		public static List<InventorySlot> Slots = new List<InventorySlot>();
+	}
+
 	public static class Crafting
 	{
 		public static CraftingRecipeManager RecipeManager;
