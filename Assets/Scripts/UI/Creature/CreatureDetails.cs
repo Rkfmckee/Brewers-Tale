d62d98ae790fe33fed08089c340bcb4104d97a20
Ryan McKeever <rkfmckee@gmail.com>
2023-03-10 20:36:45 +0000

    Add health bar to creature details

--- Assets/Scripts/UI/Creature/CreatureDetails.cs
+++ Assets/Scripts/UI/Creature/CreatureDetails.cs
@@ -20,12 +20,16 @@ public class CreatureDetails : MonoBehaviour
 	[SerializeField] private Transform creatureTitleBar;
 
 	private Creature targetCreature;
-	private float childHeightPixels;
 
 	private RectTransform rectTransform;
 	private Transform conditionGroup;
 	private new Camera camera;
 	private Camera creaturePreviewCamera;
+
+	private HealthSystem healthSystem;
+	private Transform healthBar;
+	private TextMeshProUGUI currentHealth;
+	private TextMeshProUGUI maxHealth;
 
 	#endregion
 
@@ -37,6 +41,11 @@ public class CreatureDetails : MonoBehaviour
 		camera = Camera.main;
 
 		conditionGroup = transform.Find("Conditions");
+	}
+
+	private void Update()
+	{
+		SetCreatureHealth();
 	}
 
 	private void OnDestroy()
@@ -53,12 +62,24 @@ public class CreatureDetails : MonoBehaviour
 	{
 		targetCreature = creature;
 
+		healthSystem = targetCreature.GetComponent<HealthSystem>();
+		healthBar = transform.Find("HealthBar").Find("Health");
+		currentHealth = transform.Find("HealthBar").Find("HealthText").Find("CurrentHealth").GetComponent<TextMeshProUGUI>();
+		maxHealth = transform.Find("HealthBar").Find("HealthText").Find("MaxHealth").GetComponent<TextMeshProUGUI>();
+
 		var creatureName = creature is Enemy ? (creature as Enemy).EnemyName : creature.name;
 		WorldCanvasManager.BookCanvasRight.transform.Find("Pages").Find("Creature").Find("Titlebar").Find("Title").GetComponent<TextMeshProUGUI>().SetText(creatureName);
 
 		AddConditionDetails();
 		CreateCreaturePreview();
 		ShowOtherCreatures(false);
+	}
+
+	private void SetCreatureHealth()
+	{
+		healthBar.localScale = new Vector3(healthSystem.CurrentHealth / healthSystem.MaxHealth, 1, 1);
+		currentHealth.text = healthSystem.CurrentHealth.ToString();
+		maxHealth.text = healthSystem.MaxHealth.ToString();
 	}
 
 	private void AddConditionDetails()
