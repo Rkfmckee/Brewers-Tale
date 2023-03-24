5c9a0054ac6102324e98289599077ea7a99e98f2
Ryan McKeever <rkfmckee@gmail.com>
2023-03-24 22:11:10 +0000

    Populate queue ui with current enemies

--- Assets/Scripts/Creatures/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Creatures/Enemy/EnemySpawner.cs
@@ -1,14 +1,17 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : Singleton<EnemySpawner>
 {
 	#region Fields
 
-	[SerializeField]
-	private List<GameObject> enemyQueue;
+	[SerializeField] private List<Enemy> enemyQueue;
+	[SerializeField] private GameObject queuePrefab;
 
 	private EnemySpace spawnSpace;
+	private List<GameObject> enemyQueueUI;
 
 	#endregion
 
@@ -20,10 +23,36 @@ public class EnemySpawner : Singleton<EnemySpawner>
 		if (enemyQueue.Count == 0) return;
 		if (spawnSpace.EnemyInSpace != null) return;
 
-		var enemyPrefab = enemyQueue[0];
+		var enemyPrefab = enemyQueue[0].gameObject;
 		var enemy = Instantiate(enemyPrefab, spawnSpace.transform.position, enemyPrefab.transform.rotation);
 
 		enemyQueue.RemoveAt(0);
+		CreateQueueUI();
+	}
+
+	public void CreateQueueUI()
+	{
+		if (enemyQueueUI != null)
+		{
+			// Delete previous queue
+			foreach (var enemyUI in enemyQueueUI)
+			{
+				Destroy(enemyUI);
+			}
+		}
+
+		enemyQueueUI = new List<GameObject>();
+		var queue = WorldCanvasManager.Instance.EnemyQueue;
+
+		for (int i = 0; i < enemyQueue.Count; i++)
+		{
+			var enemy = enemyQueue[i];
+			var enemyInQueue = Instantiate(queuePrefab, queue).transform;
+			enemyInQueue.Find("Image").GetComponent<Image>().sprite = enemy.QueueIcon;
+			enemyInQueue.Find("Name").GetComponent<TextMeshProUGUI>().text = $"{i + 1}. {enemy.EnemyName}";
+
+			enemyQueueUI.Add(enemyInQueue.gameObject);
+		}
 	}
 
 	#endregion
